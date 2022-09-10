6e511fe7d36582400ba56d401c38e00deea81699
Cyb3rtron <92073651+light-y4gami@users.noreply.github.com>
2022-09-10 16:01:38 -0400

    Add files via upload

---
+++ ejercicio_2_solucion.js
@@ -0,0 +1,23 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Cambia solo el código debajo de esta línea
+const wordBlanks =
+  "My " +
+  myNoun +
+  " " +
+  myAdjective +
+  " " +
+  "the other day" +
+  " " +
+  myVerb +
+  " " +
+  "with" +
+  " " +
+  myAdverb; // Cambia esta línea
+// Cambia solo el código encima de esta línea
+
+console.log(wordBlanks);
+
